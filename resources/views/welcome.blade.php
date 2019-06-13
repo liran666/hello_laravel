39d1bff2019d5f1ebbefd94adf2ca17b7bfc1370
liran <377586819@qq.com>
2019-06-13 02:28:19 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel -- byliran
+                    Laravel --- byliran
                 </div>
             </div>
         </div>
